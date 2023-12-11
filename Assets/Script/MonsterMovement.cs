f076b6f41bf836f2ee1fab571d9417b11835de78
fatin farhana <f.fatinfarhanamasud@gmail.com>
2023-12-11 16:36:17 +0800

    done cleaning UI

--- Assets/Script/MonsterMovement.cs
+++ Assets/Script/MonsterMovement.cs
@@ -4,32 +4,67 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
-    public Transform[] patrolPoints;
-    public float moveSpeed;
-    public int patrolDestination;
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private CompositeCollider2D compositeCollider;
 
-    // Update is called once per frame
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isFlying", true);
+
+        compositeCollider = GetComponent<CompositeCollider2D>();
+    }
+
     void Update()
     {
-        if (patrolDestination == 0)
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
         {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[0].position) < 0.2f)
-            {
-                transform.localScale = new Vector3(1, 1, 1);
-                patrolDestination = 1;
-            }
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
         }
 
-        if (patrolDestination == 1)
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
         {
-            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed * Time.deltaTime);
-            if (Vector2.Distance(transform.position, patrolPoints[1].position) < 0.2f)
-            {
-                transform.localScale = new Vector3(-1, 1, 1);
-                patrolDestination = 0;
-            }
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
         }
     }
 
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Bounds bounds = compositeCollider.bounds;
+
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+
+
+        //Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        //Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        //Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+
+    }
 }
